4745756728b774d5fe4336656bd7437a62ee21a6
EddyM <Eddy.Mendoza01@student.csulb.edu>
2022-06-20 20:11:26 -0700

    1:39:28

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Button } from 'react-native';
-import { TextInput } from 'react-native-web';
+import { TextInput } from 'react-native';
 
 export default function App() {
+function goalInputHandler(){};
+
+function addGoalHandler(){};
+
   return (
     
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-      <TextInput style={styles.TextInput} placeholder='Your Course Goal' />
+      <TextInput style={styles.textInput} placeholder='Your Course Goal' />
       <Button title ="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text> List of goals...</Text>
       </View>
     </View>
@@ -18,22 +22,34 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  
+   
   appContainer:{
+    flex:1,
     padding:50,
+    paddingHorizontal:16,
   },
 
   inputContainer:{
+    flex:1,
     flexDirection:'row',
-    justifyContent:'space-between'
+    justifyContent:'space-between',
+    alignItems:'center',
+    //paddingBottom:24,
+    marginBottom:24,
+    borderBottomWidth:1,
+    borderBottomColor:'lightgray',
 
   }, 
-  TextInput:{
+  textInput:{
       borderWidth:1,
       borderColor:'lightgray',
-      width: '80%',
+      width: '70%',
       marginRight:8,
       padding:8,
 
+  },
+  goalsContainer:{
+
+    flex:5,
   }
 });
